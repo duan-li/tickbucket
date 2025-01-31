18b57ca12a9f430105ce38644df0bd269f01d207
DL <inputx@gmail.com>
2025-01-31 14:50:14 +1100

    create login compontent and login ui

---
+++ src/app/Login.tsx
@@ -0,0 +1,12 @@
+// Login compontent (UI).
+
+export const Login = () => {
+  return (
+    <form>
+      <article style={{ maxWidth: "420px", margin: "auto" }}>
+        <header>Login</header>
+        <strong>Hello Login!</strong>
+      </article>
+    </form>
+  );
+};
