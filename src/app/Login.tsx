22db3710c99ff3948c95cfeee733c9e2c7b9a89a
DL <inputx@gmail.com>
2025-01-31 22:54:53 +1100

    finish login page and login compontent work together.

--- src/app/Login.tsx
+++ src/app/Login.tsx
@@ -1,11 +1,49 @@
 // Login compontent (UI).
+"use client";
+import Link from "next/link";
 
-export const Login = () => {
+export const Login = ({ isPasswordLogin }) => {
   return (
     <form>
-      <article style={{ maxWidth: "420px", margin: "auto" }}>
+      <article style={{ maxWidth: "480px", margin: "auto" }}>
         <header>Login</header>
-        <strong>Hello Login!</strong>
+        <fieldset>
+          <label htmlFor="email">
+            Email
+            <input type="email" id="email" name="email" required />
+          </label>
+          {isPasswordLogin && (
+            <label htmlFor="password">
+              Password
+              <input type="password" id="password" name="password" />
+            </label>
+          )}
+        </fieldset>
+        <p>
+          {isPasswordLogin ? (
+            <Link
+              href={{
+                pathname: "/login",
+                query: { magicLink: "yes" },
+              }}
+            >
+              Go to Magic Link Login
+            </Link>
+          ) : (
+            <Link
+              href={{
+                pathname: "/login",
+                query: { magicLink: "no" },
+              }}
+            >
+              Go to Password Login
+            </Link>
+          )}
+        </p>
+        <button type="submit">
+          Sign in with
+          {isPasswordLogin ? " Password" : " Magic Link"}
+        </button>
       </article>
     </form>
   );
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 import { Login } from "../Login";
 
-export default function LoginPage() {
-  return <Login />;
+export default function LoginPage({ searchParams }) {
+  const wantsMagicLink = searchParams.magicLink === "yes";
+  return <Login isPasswordLogin={!wantsMagicLink} />;
 }
