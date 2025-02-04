5306d7c1dff70a3cd50dbea996f561b1cfa9ea9f
DL <inputx@gmail.com>
2025-02-04 15:07:31 +1100

    update middleware Protecting access to the Ticket Management system

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,18 @@
 import { getSupabaseReqResClient } from "@/supabase-utils/reqResClient";
 import { NextResponse } from "next/server";
 
-export async function middleware(request) {
-  const { supabase, response } = getSupabaseReqResClient({ request });
-  await supabase.auth.getSession();
+export async function middleware(req) {
+  const request = NextResponse.next();
+  const { supabase, response } = getSupabaseReqResClient({ request: request });
+  const session = await supabase.auth.getSession();
+  const requestedPath = req.nextUrl.pathname;
+  const sessionUser = session.data?.session?.user;
+
+  if (requestedPath.startsWith("/tickets")) {
+    if (!sessionUser) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  }
   return response.value;
 }
 
