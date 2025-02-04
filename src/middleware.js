23dbbfa2db2672d501227559ff57070bf3a09c33
DL <inputx@gmail.com>
2025-02-04 14:36:47 +1100

    change middleware by initializing the Supabase client with our getSupabaseReqResClient function, and adding another line waiting for supabase.auth.getSession() to finish

--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,10 @@
+import { getSupabaseReqResClient } from "@/supabase-utils/reqResClient";
 import { NextResponse } from "next/server";
 
-export async function middleware(req) {
-  const res = NextResponse.next();
-  return res;
+export async function middleware(request) {
+  const { supabase, response } = getSupabaseReqResClient({ request });
+  await supabase.auth.getSession();
+  return response.value;
 }
 
 export const config = {
