87b89b5628409b61c57b071a5a6d478f78624b1b
DL <inputx@gmail.com>
2025-02-24 22:06:05 +1100

    form validation > > Processing the registration with a Route Handler

--- src/app/[tenant]/auth/register/route.ts
+++ src/app/[tenant]/auth/register/route.ts
@@ -1,3 +1,6 @@
+import { NextResponse } from "next/server";
+import { buildUrl } from "@/utils/url-helpers";
+import { getSupabaseAdminClient } from "@/supabase-utils/adminClient";
 
 export async function POST(request, { params }) {
     const formData = await request.formData();
@@ -5,5 +8,33 @@ export async function POST(request, { params }) {
     const email = formData.get("email");
     const password = formData.get("password");
     const tenant = params.tenant;
-    return Response.json({ email, password, tenant });
+
+    const isNonEmptyString = (value) =>
+        typeof value === "string" && value.trim().length > 0;
+    const emailRegex = /^\S+@\S+$/; // simple front@back regex
+    if (
+        !isNonEmptyString(name) ||
+        !isNonEmptyString(email) ||
+        !emailRegex.test(email) ||
+        !isNonEmptyString(password)
+    ) {
+        return NextResponse.redirect(
+            buildUrl("/error", tenant, request),
+            302
+    );
+    }
+
+    const [ , emailHost] = email.split('@');
+
+    const supabaseAdmin = getSupabaseAdminClient();
+    const { data, error } = await supabaseAdmin
+        .from("tenants")
+        .select("*")
+        .eq("id", tenant)
+        .eq("domain", emailHost)
+        .single();
+      
+    return Response.json({ email, password, tenant, emailHost });
+
+    // return Response.json({ email, password, tenant });
 }
