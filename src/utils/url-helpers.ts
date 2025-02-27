779b8cc37e72cd6a63d96b32152b17ce359cc237
DL <inputx@gmail.com>
2025-02-27 23:05:48 +1100

    make tenant domain working for test

--- src/utils/url-helpers.ts
+++ src/utils/url-helpers.ts
@@ -1,6 +1,20 @@
 export function urlPath(applicationPath, tenant) {
-return `/${tenant}${applicationPath}`;
+    return `/${tenant}${applicationPath}`;
 }
 export function buildUrl(applicationPath, tenant, request) {
-return new URL(urlPath(applicationPath, tenant), request.url);
+    return new URL(urlPath(applicationPath, tenant), request.url);
+}
+
+export function getHostnameAndPort(request) {
+    const hostnameWithPort = request.headers.get("host");
+    const [realHostname, port] = hostnameWithPort.split(":");
+
+    let hostname;
+    if (process.env.OVERRIDE_TENANT_DOMAIN) {
+        hostname = process.env.OVERRIDE_TENANT_DOMAIN;
+    } else {
+        hostname = realHostname;
+    }
+
+    return [hostname, port];
 }
