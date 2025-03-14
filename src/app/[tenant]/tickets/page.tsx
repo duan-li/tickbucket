18de7f233c0f618bd9807faafdf727a15dba7f48
DL <inputx@gmail.com>
2025-03-14 21:56:12 +1100

    Creating a ticket filter elements < Listing and filtering tickets

--- src/app/[tenant]/tickets/page.tsx
+++ src/app/[tenant]/tickets/page.tsx
@@ -1,9 +1,11 @@
 import { TicketList } from "./TicketList";
+import { TicketFilters } from "./TicketFilters";
 
 export default function TicketListPage({params, searchParams}) {
   return (
     <>
       <h2>Ticket List</h2>     
+      <TicketFilters tenant={params.tenant} />
       <TicketList tenant={params.tenant} searchParams={searchParams} />
     </>
   );
